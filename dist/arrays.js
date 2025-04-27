0bdc27d97c8eb106d1e35cd5c2fa4f62964d370f
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-27 12:01:41 +0500

    added tuples

--- dist/arrays.js
+++ dist/arrays.js
@@ -1,6 +1,11 @@
 "use strict";
+// arrays
 let marks = [90, 56, 97, 23, 89];
 let ar1 = ["hh", "aka", "u", 9];
 ar1.push(false); // not allowed
 let arr2 = []; // array of any type
 arr2.push([90, 78]);
+// tuples
+let student = [["Muhammad Tayyab", 12]];
+student.push();
+let st1 = ["Ali Raza"];
