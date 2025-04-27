3c0ed31540e83cbe5fee32a56daa1c94c461161f
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-27 10:24:59 +0500

    practiced type safety for string, any, unknown, boolean variables

--- src/variables_type_safety.ts
+++ src/variables_type_safety.ts
@@ -13,3 +13,22 @@ let avg = 6.8; // now its type is number we cant change data type of this variab
 let parcels: 90 = 90;
 // in case we are using large numbers we can separate them using numeric separators like this:
 let hight = 1_000_000_000;
+// String
+const myName = "Muhammad Tayyab";
+let firstName: string = myName.slice(0, myName.indexOf(" ") + 1);
+console.log(firstName);
+// Boolean
+let isValid = true;
+const isAdult = false;
+let idVerified: boolean = isValid && isAdult;
+// Any - The God Father
+// By using any you are letting ts of the type safety now it works same like js
+let ground: any = "Football Ground";
+ground = 78;
+console.log(ground.toExponential());
+// Avoid it if its possible
+// Unknown - Same to Any but different
+// By using unknown you are also letting ts of the type safety but not if you want to use methods related to specific type like this:
+let email: unknown = "tayyabpasha1918@gmail.com"; // here as we annotated its as unknown but we initialized it with string now we can't use string specific methods on email as it's type is unknown to do that we to first explicitly tell ts it's original type.
+let tls: string = email.slice(email.indexOf("@")); // not allowed as email is unknown
+if (typeof email === "string") tls = email.slice(email.indexOf("@")); // now it works
