19652adf643cf9337e4a4ff1ad362ae6323161bb
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-27 11:43:12 +0500

    practiced arrays

---
+++ src/arrays.ts
@@ -0,0 +1,6 @@
+let marks = [90, 56, 97, 23, 89];
+type User = (string | number)[];
+let ar1 = ["hh", "aka", "u", 9];
+ar1.push(false); // not allowed
+let arr2 = []; // array of any type
+arr2.push([90, 78]);
